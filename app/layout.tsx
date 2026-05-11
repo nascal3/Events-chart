96f7f9ce7d1cf994ba05aa45c8f64a5c459e0701
caleb <nascal3@gmail.com>
2026-05-11 16:16:23 +0300

    Set up top layout of the site

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
-import { Schibsted_Grotesk, Martian_Mono } from "next/font/google";
+import { Schibsted_Grotesk, Martian_Mono, Geist } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+import LightRays from "@/app/components/LightRays";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 const schibstedGrotesk = Schibsted_Grotesk({
   variable: "--font-schibsted-grotesk",
@@ -25,9 +29,28 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${schibstedGrotesk.variable} ${martianMono.variable} min-h-screen antialiased`}
+      className={cn("min-h-screen", "antialiased", schibstedGrotesk.variable, martianMono.variable, "font-sans", geist.variable)}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body>
+        <div className="absolute inset-0 top-0 z-[-1] min-h-screen">
+          <LightRays
+              raysOrigin="top-center"
+              raysColor="#5dfeca"
+              raysSpeed={0.5}
+              lightSpread={0.9}
+              rayLength={1.4}
+              followMouse={true}
+              mouseInfluence={0.01}
+              noiseAmount={0.0}
+              distortion={0.01}
+              pulsating={false}
+              fadeDistance={1}
+              saturation={1}/>
+        </div>
+        <main>
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 const Page = () => {
     return (
-        <div>Page</div>
+        <section>
+            <h1 className="text-center">The Hub for Every dev Event <br /> You have every event you Need</h1>
+            <p className="text-center mt-5">Hackathons, Meetups, Workshops, and more</p>
+        </section>
     )
 }
 export default Page
