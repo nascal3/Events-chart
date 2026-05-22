a2ae86feecbda7e1a0f3b5686301e15b42cb9728
caleb <nascal3@gmail.com>
2026-05-22 16:05:52 +0300

    Integrated Cloudinary for image upload in event creation API and added support for retrieving events. Updated dependencies and Next.js configuration for remote image handling.

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,6 +1,7 @@
 import {NextRequest, NextResponse} from "next/server";
 import {connectToDB} from "@/lib/mongodb";
 import Event from "@/database/event.model";
+import { v2 as cloudinary } from "cloudinary";
 
 export async function POST(req: NextRequest) {
   try {
@@ -16,6 +17,23 @@ export async function POST(req: NextRequest) {
           return NextResponse.json({ message: 'Invalid JSON format data', error: error instanceof Error ? error.message : 'Unknown error' }, { status: 400 });
       }
 
+      const file = formData.get('image') as File;
+      if (!file) {
+        return NextResponse.json({ message: 'Image is required' }, { status: 400 });
+      }
+
+      const arrayBuffer = await file.arrayBuffer();
+      const buffer = Buffer.from(arrayBuffer);
+
+      const uploadResult = await new Promise((resolve, reject) => {
+        cloudinary.uploader.upload_stream({resource_type: "image", folder: "event_images",}, (error, results) => {
+            if (error) return reject(error);
+            resolve(results);
+        }).end(buffer);
+      });
+
+      event.image = (uploadResult as { secure_url: string }).secure_url;
+
       const createdEvent = await Event.create(event);
 
        return  NextResponse.json({ message: 'Event created successfully', event: createdEvent }, { status: 201 });
@@ -24,4 +42,15 @@ export async function POST(req: NextRequest) {
       console.error(error);
       return NextResponse.json({ message: 'Event creation failed', error: error instanceof Error ? error.message : 'Unknown error' });
   }
+}
+
+export async function GET() {
+    try {
+        await connectToDB();
+        const events = await Event.find().sort({ createdAt: -1 });
+        return NextResponse.json({ events }, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({ message: 'Event retrieval failed', error: error instanceof Error ? error.message : 'Unknown error' });
+    }
+
 }
