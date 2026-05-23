710e8e0257ca4855bfe4f0c296dd6e1517d33ad7
caleb <nascal3@gmail.com>
2026-05-23 10:21:53 +0300

    Added event details page with dynamic slug-based rendering and updated utility function to handle IEvent type

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,10 +1,11 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import {IEvent} from "@/database/event.model";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export function parseStringify(value: any) {
+export function parseStringify(value: IEvent) {
   return JSON.parse(JSON.stringify(value));
 }
